e049b49b6ad398254c42f4de0b8293441a3e6740
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-10 20:57:22 +0000

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
-#include<errno.h>
-
+#include <unistd.h>
 /**
  * main - stderr
  *
- * Return: Always 1 (Fail)
+ * Return:1
  */
 int main(void)
 {
-	errno=-1;
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
