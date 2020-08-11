8d6b94693efc9133016a9a65936adc5957c5c0c5
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-11 09:30:12 +0000

    fixing char in 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char i, j;
+	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
-		for (j = '0'; j <= '9'; j++)
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 9; j++)
 		{
-			putchar(i);
-			putchar(j);
-			if ((i != '9') || (j != '9'))
+			putchar('0' + i);
+			putchar('0' + j);
+			if ((i != 9) || (j != 9))
 			{
 				putchar(',');
 				putchar(' ');
