a6e81c8c515a85d197135222afd67e374036a65d
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-13 15:09:03 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,15 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 97; i++)
-		printf("%d, ", i);
+	if (n <= 98)
+	{
+		for (i = n; i <= 97; i++)
+			printf("%d, ", i);
+	}
+	else
+	{
+		for (i = n; i >= 97; i--)
+			printf("%d, ", i);
+	}
 	printf("98\n");
 }
