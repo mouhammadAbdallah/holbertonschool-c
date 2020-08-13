adf0be5e6fb6705c9a8b04fb8afc9f40ef4f6d80
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-13 12:11:44 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - alphabet
+ *
+ * void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar('a' + j);
+		}
+		_putchar('\n');
+	}
+}
