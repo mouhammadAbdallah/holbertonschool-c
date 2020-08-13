f370e19f05d91d727d298b272763cde2b1dbe455
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-13 18:49:38 +0000

    task 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 98 fibonacci numbers.
+ * Return: Nothing.
+ */
+int main(void)
+{
+	unsigned long count, i, j, k;
+
+	i = 0;
+	j = 1;
+	for (count = 0; count < 98; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu", k);
+		if (count == 97)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
