e115be95ad934fd4900b78c537194ab61b2c53b8
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-13 17:48:01 +0000

    task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int i, s=0;
+	int i, s = 0;
 
 	for (i = 1; i < 1024; i++)
 		if (i % 3 == 0 || i % 5 == 0)
 			s += i;
-	printf("%d\n",s);
+	printf("%d\n", s);
 	return (0);
 }
