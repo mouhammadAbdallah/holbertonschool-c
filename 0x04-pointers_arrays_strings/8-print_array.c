fb2676ad07d2d694d1688545b8d4d423bada9625
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-17 23:02:02 +0300

    fix 8

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
 #include <stdio.h>
+#include "holberton.h"
 
 /**
  * print_array - array
- * @a: array
- * @n: number
+ * @a: array name
+ * @n: is the number of elements
  *
- * void
+ * Return: a and n inputs
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	if (n <= 0 || a == NULL)
-		return;
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < (n - 1); i++)
 		printf("%d, ", a[i]);
-	printf("%d\n", a[n - 1]);
+		if (i == (n - 1))
+			printf("%d", a[n - 1]);
+	printf("\n");
 }
