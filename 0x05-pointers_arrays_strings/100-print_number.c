ed04e95b15c13788550a27b7df84e7bcdb7f524f
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-18 16:13:50 +0300

    task 100 fix

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_number - print number recursive
+ * print_number - print number
  * @n: number
 *
 * void
@@ -9,14 +9,37 @@
 
 void print_number(int n)
 {
+	int i, k, j = 1, tmp = n;
+
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
+		/*n = n * -1;*/
 	}
-	if (n / 10)
+
+	while (tmp / 10 != 0)
 	{
-		print_number(n / 10);
+		tmp = tmp / 10;
+		j = j * 10;
 	}
-	_putchar('0' + n % 10);
+	k = j;
+	for (i = 1; i <= k; i = i * 10)
+	{
+		if (n > 0)
+		{
+			if (j != 1)
+				_putchar(n / j + '0');
+			else
+				_putchar(n + '0');
+		}
+		else
+		{
+			if (j != 1)
+				_putchar(-(n / j) + '0');
+			else
+				_putchar(-n + '0');
+		}
+		n = n % j;
+		j = j / 10;
+	}
 }
