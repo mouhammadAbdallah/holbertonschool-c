29a50f44f11c0eb635d265c5c02ece6d234e267a
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-24 15:47:24 +0300

    task 4

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - power
+ * @x: number
+ * @y: number
+ *
+ * Return: x^y or -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
