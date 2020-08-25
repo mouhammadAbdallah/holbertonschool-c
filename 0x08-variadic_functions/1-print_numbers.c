de25325de74945d447b0de034b3f4a87f24fe314
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-25 18:37:57 +0300

    task 2

---
+++ 0x08-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print nbs
+ * @separator: str separator
+ *
+ * void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	if (n == 0)
+		return;
+	va_start(arg, n);
+	if (!separator)
+		separator = "";
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d%s", va_arg(arg, int), separator);
+	}
+	printf("%d\n", va_arg(arg, int));
+	va_end(arg);
+}
