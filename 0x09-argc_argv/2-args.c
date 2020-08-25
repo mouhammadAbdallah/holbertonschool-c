7c31462b2e6a214d14b7b5ba07b64121e9a7d432
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-25 09:08:14 +0300

    task 2

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -2,14 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - print arg number
+ * main - print args
  * @argc: argc
  * @argv: arguments
  *
  * Return: 0 (Success)
  */
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char **argv)
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
