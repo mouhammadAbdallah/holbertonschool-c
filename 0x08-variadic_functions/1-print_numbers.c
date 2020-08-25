503488f8a32d7d49257b79aac0eb476b32b575cf
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-25 18:46:17 +0300

    task 2

--- 0x08-variadic_functions/1-print_numbers.c
+++ 0x08-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (n == 0)
 		return;
 	va_start(arg, n);
-	if (!separator)
+	if (separator==NULL)
 		separator = "";
 	for (i = 0; i < n - 1; i++)
 	{
---
+++ 0x08-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print str
+ * @separator: str separator
+ * @n: count
+ *
+ * void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+	char *s;
+
+	if (n == 0)
+		return;
+	va_start(arg, n);
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n - 1; i++)
+	{
+		s = va_arg(arg, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s%s", s, separator);
+	}
+	s = va_arg(arg, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s\n", s);
+	va_end(arg);
+}
