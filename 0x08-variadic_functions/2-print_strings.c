846650b5168b95dda6338ceda8bcf5187b04851f
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-25 18:48:04 +0300

    task 2

--- 0x08-variadic_functions/2-print_strings.c
+++ 0x08-variadic_functions/2-print_strings.c
@@ -18,12 +18,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (n == 0)
 		return;
 	va_start(arg, n);
-	if (separator == NULL)
+	if (separator[0] == '\0')
 		separator = "";
 	for (i = 0; i < n - 1; i++)
 	{
 		s = va_arg(arg, char *);
-		if (s == NULL)
+		if (s[0] == '\0')
 			s = "(nil)";
 		printf("%s%s", s, separator);
 	}
