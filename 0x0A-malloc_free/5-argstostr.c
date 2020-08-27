4fdd1a50247664e8e9d12e73f0de8f8c1518aaf4
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-27 13:54:46 +0300

    fix 5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -2,21 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - str len
- * @s: a pointer
- *
- * Return: len
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
  * argstostr - concatenate strs
  * @ac: count
  * @av: arr of strs
@@ -25,27 +10,34 @@ int _strlen(char *s)
  */
 char *argstostr(int ac, char **av)
 {
-	int l = 0, i, j, k = 0;
-	char *s;
+	char *str;
+	int arg, byte, index, size = ac;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
-		l += _strlen(av[i]);
-	s = (char *)malloc(sizeof(char) * (l + 1));
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; j < _strlen(av[i]); j++)
-		{
 
-			s[k] = av[i][j];
-			k++;
-		}
-		s[k] = '\n';
-		k++;
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
 	}
-	s[k] = '\0';
-	return (s);
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
 }
