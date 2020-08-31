ed05e49416a62c58824eeb8ede6118619cd88a23
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-31 21:30:45 +0300

    fixing NULL task 1

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -26,26 +26,23 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int l, i, l1, l2;
+	unsigned int l, i, l1, l2, j;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	if (!s2)
+	if (s2 == NULL)
 		s2 = "";
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
 	n = (l2 >= n) ? n : l2;
 	l = l1 + n + 1;
 	s = malloc(sizeof(*s) * l);
-	if (!s)
+	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < l - 1; i++)
-	{
-		if (i < l1)
-			s[i] = s1[i];
-		else
-			s[i] = s2[i - l1];
-	}
-	s[l] = '\0';
+	for (i = 0; i < l1; i++)
+		s[i] = s1[i];
+	for (j = 0; j < n; i++, j++)
+		s[i] = s2[j];
+	s[i] = '\0';
 	return (s);
 }
