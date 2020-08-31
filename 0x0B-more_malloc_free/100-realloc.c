e7b2956a40d4d01241b2299c9ab81c3c9d4504b2
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-08-31 22:32:55 +0300

    task 100

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -30,6 +30,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		tmp = ptr;
 		l = (new_size < old_size) ? new_size : old_size;
 		buffer = malloc(new_size);
+		if (buffer == NULL)
+			return (NULL);
 		for (i = 0; i < l; i++)
 			buffer[i] = tmp[i];
 		free(ptr);
