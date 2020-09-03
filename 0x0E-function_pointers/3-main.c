8bed290178b706cac246f04974f0592c2085277e
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-03 12:38:43 +0300

    task 3

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -11,18 +11,17 @@ int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(98);
 	}
-	if (!(argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*'
-		|| argv[2][0] == '/' || argv[2][0] == '%'))
+	if (!(argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(100);
 	}
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
