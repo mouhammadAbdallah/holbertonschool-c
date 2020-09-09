fe167c16d527494e479d6ce65d27c97aed910457
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-09 12:37:15 +0300

    task 2

--- 0x0F-bit_manipulation/2-get_bit.c
+++ 0x0F-bit_manipulation/2-get_bit.c
@@ -11,6 +11,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
 	while (i < index && n > 1)
 	{
 		n = n >> 1;
