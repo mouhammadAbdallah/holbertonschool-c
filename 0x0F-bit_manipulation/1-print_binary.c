d8a473d9ce96ba7d2a589b4e6e79b0b0a210605f
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-09 12:18:30 +0300

    fix putchar task 1

--- 0x0F-bit_manipulation/1-print_binary.c
+++ 0x0F-bit_manipulation/1-print_binary.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
 
 /**
- * print_binary - print_binary
- * @n: unsigned long int
+ *print_binary-prints the binary
+ *@n: unsigned long int is n that count
  *
- * void
  */
+
 void print_binary(unsigned long int n)
 {
-	if (n == 0 || n == 1)
-	{
-		_putchar('0' + n);
-		return;
-	}
-	print_binary(n / 2);
-	_putchar('0' + n % 2);
+
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
 }
