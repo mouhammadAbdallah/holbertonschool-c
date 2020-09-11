a009f2cb714818cc3bf9883186237faa7028dc65
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-11 22:07:23 +0300

    task 1

--- 0x10-file_io/1-create_file.c
+++ 0x10-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		*text_content = '\0';
 	for (len = 0; text_content[len]; len++)
 		;
-	openn = open(filename, O_CREAT | O_WRONLY, 0600);
+	openn = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	writee = write(openn, text_content, len);
 	if (openn == -1 || writee == -1)
 		return (-1);
