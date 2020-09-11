8eb338ee6323d97d39339a2e01b672b04e717e2e
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-11 22:04:35 +0300

    task 1

---
+++ 0x10-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * create_file - create_file
+ * @filename: filename
+ * @text_content: str
+ *
+ * Return: -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t openn, writee, len;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		*text_content = '\0';
+	for (len = 0; text_content[len]; len++)
+		;
+	openn = open(filename, O_CREAT | O_WRONLY, 0600);
+	writee = write(openn, text_content, len);
+	if (openn == -1 || writee == -1)
+		return (-1);
+	close(openn);
+
+	return (1);
+}
