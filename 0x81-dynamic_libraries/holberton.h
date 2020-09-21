f79b798291874762e6efd5e2d48ac855d25b9640
Mohamad Abdallah <mohamad1031998@gmail.com>
2020-09-21 22:09:52 +0200

    task 0

--- 0x81-dynamic_libraries/holberton.h
+++ 0x81-dynamic_libraries/holberton.h
@@ -1,4 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdlib.h>
+#include <stdio.h>
+
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,3 +26,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+#endif /* HOLBERTON_H */
