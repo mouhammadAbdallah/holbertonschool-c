a42a9f7c3992962a85babeae40174207482815c5
Mohamad ABDALLAH <mohamad@Mohamad.localdomain>
2020-10-13 21:33:42 +0200

    task 0

---
+++ 0x13-signals/0-handle_signal.c
@@ -0,0 +1,29 @@
+#include "signals.h"
+
+/**
+ * sig_handler - the handler that prints
+ * when signal SIGINT of Ctrl-C is recieved
+ * @signo: signal number
+ * Return: 0 on success, or -1 on error
+ */
+void sig_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		signal(SIGINT, sig_handler);
+		printf("Gotcha! [<%d>]", SIGINT);
+	}
+}
+
+/**
+ * handle_signal - a function that set a handler
+ * for the signal SIGINT of Ctrl-C
+ *
+ * Return: 0 on success, or -1 on error
+ */
+int handle_signal(void)
+{
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+		return (-1);
+	return (0);
+}
---
+++ 0x13-signals/signals.h
@@ -0,0 +1,10 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int handle_signal(void);
+
+#endif /* SIGNALS_H */
